a5ce05b9495557b6feac53dcdc206cfdb6669f8d
rachelchan0324 <63142824+rachelchan0324@users.noreply.github.com>
2023-11-15 00:23:38 -0800

    Add files via upload

--- render.cpp
+++ render.cpp
@@ -5,40 +5,56 @@
 
 using namespace std;
 
+void printWords(char word[], int& count, bool& firstWordInLine, int lineLength, ostream& outf){
+    if(firstWordInLine && strlen(word) < lineLength){
+        outf << word;
+        firstWordInLine = false;
+    }
+    else if(count + 1 < lineLength){
+        outf << " " << word;
+        count++;
+    }
+    else if(firstWordInLine && strlen(word) == lineLength){
+        outf << word << endl;
+        count = 0;
+    }
+    // if the word perfectly completes the line, print a new line
+    else if(count + 1 == lineLength){
+        outf << " " << word << endl;
+        count = 0;
+        firstWordInLine = true;
+    }
+    // the word will not fit in the current line, print a new line and print the word in the next line
+    else{
+        outf << endl << word;
+        count = 0;
+        count += strlen(word);
+    }
+    // reset word
+    for(int i = 0; word[i] != '\0'; i++){
+        word[i] = '\0';
+    }
+}
+
 int render(int lineLength, istream& inf, ostream& outf){
     if(lineLength < 1)
         return 2;
     char c;
     char word[181];
     int count = 0;
+    bool firstWordInLine = true;
     while(inf.get(c)){ // read in one char at a time
         if(!isspace(c)){
             word[strlen(word)] = c; // append char c to the current word
             count++;
         }
-        else{ // if the program reaches a space character, print word and reset it to get ready for the next word
-            if(strlen(word) != 0){
-                if(count + 1 + strlen(word) <= lineLength){
-                    outf << word << " ";
-                    count++;
-                }
-                else{
-                    if(count + strlen(word) <= lineLength){
-                        outf << word << endl;
-                        count = 0;
-                    }
-                    else{
-                        outf << endl << word << " ";
-                        count = 0;
-                    }
-                }
-            }
-            for(int i = 0; word[i] != '\0'; i++){
-                word[i] = '\0';
-            }
+        else{
+            if(strlen(word) != 0)
+                printWords(word, count, firstWordInLine, lineLength, outf);
         }
     }
-    outf << word << endl; // print the last word (it is not printed in the loop because our loop only prints after it reaches a space character)
+    printWords(word, count, firstWordInLine, lineLength, outf); // print the last word (it is not printed in the loop because our loop only prints after it reaches a space character)
+    outf << endl;
     return 0;
 }
 
