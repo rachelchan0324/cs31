818d525d2fee042709395701722a9f70940be55d
rachelchan0324 <63142824+rachelchan0324@users.noreply.github.com>
2023-11-15 00:45:47 -0800

    Add files via upload

--- render.cpp
+++ render.cpp
@@ -5,30 +5,47 @@
 
 using namespace std;
 
-void printWords(char word[], int& count, bool& firstWordInLine, int lineLength, ostream& outf){
-    if(firstWordInLine && strlen(word) < lineLength){
+void printWords(char word[], int& count, bool& firstWordInLine, int lineLength, ostream& outf, bool& pBreak){
+    // paragraph break
+    if(strcmp(word, "@P@") == 0){
+        if(!pBreak){
+            outf << endl << endl;
+            count = 0;
+            firstWordInLine = true;
+            pBreak = true;
+        }
+    }
+    // if this is the first word in the current line, do not print a space before it
+    else if(firstWordInLine && strlen(word) < lineLength){
         outf << word;
         firstWordInLine = false;
+        pBreak = false;
     }
+    // if the word can fit in the current line, print it
     else if(count + 1 < lineLength){
         outf << " " << word;
         count++;
+        pBreak = false;
     }
+    // if this is the first word in the current line AND it completely complets the line print, do not print a space before it and print a new line
     else if(firstWordInLine && strlen(word) == lineLength){
         outf << word << endl;
         count = 0;
+        pBreak = false;
     }
     // if the word perfectly completes the line, print a new line
     else if(count + 1 == lineLength){
         outf << " " << word << endl;
         count = 0;
         firstWordInLine = true;
+        pBreak = false;
     }
     // the word will not fit in the current line, print a new line and print the word in the next line
     else{
         outf << endl << word;
         count = 0;
         count += strlen(word);
+        pBreak = false;
     }
     // reset word
     for(int i = 0; word[i] != '\0'; i++){
@@ -43,6 +60,7 @@ int render(int lineLength, istream& inf, ostream& outf){
     char word[181];
     int count = 0;
     bool firstWordInLine = true;
+    bool pBreak = false;
     while(inf.get(c)){ // read in one char at a time
         if(!isspace(c)){
             word[strlen(word)] = c; // append char c to the current word
@@ -50,10 +68,10 @@ int render(int lineLength, istream& inf, ostream& outf){
         }
         else{
             if(strlen(word) != 0)
-                printWords(word, count, firstWordInLine, lineLength, outf);
+                printWords(word, count, firstWordInLine, lineLength, outf, pBreak);
         }
     }
-    printWords(word, count, firstWordInLine, lineLength, outf); // print the last word (it is not printed in the loop because our loop only prints after it reaches a space character)
+    printWords(word, count, firstWordInLine, lineLength, outf, pBreak); // print the last word (it is not printed in the loop because our loop only prints after it reaches a space character)
     outf << endl;
     return 0;
 }
